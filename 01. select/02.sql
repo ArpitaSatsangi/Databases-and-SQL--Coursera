54baa2b57de004cdec58db902e01a367020556b7
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-09-30 18:40:30 +0530

    Update

---
+++ 01. select/02.sql
@@ -0,0 +1,10 @@
+/*In this example, now we want to retrieve selective details of all the film records. Let us retrieve the names of all the films along with director names and writer names.
+
+Problem:
+
+Retrieve the names of all films with director names and writer names.
+
+Solution:
+*/
+
+SELECT Title, Director, Writer FROM FilmLocations;
