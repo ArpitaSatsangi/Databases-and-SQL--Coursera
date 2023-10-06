37506ecc45364ebefb6339ae4a30354f8f2f3617
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-10-06 18:42:12 +0530

    added

--- 11. String Patterns, Sorting and Grouping/02.sql
+++
@@ -1,5 +0,0 @@
-/* Retrieve all employees whose address is in Elgin,IL.
-*/
-SELECT F_NAME , L_NAME
-FROM EMPLOYEES
-WHERE ADDRESS LIKE '%Elgin,IL%';
--- 11. String Patterns, Sorting and Grouping/03.sql
+++
@@ -1,5 +0,0 @@
-/*Retrieve all employees who were born during the 1970’s.
-*/
-SELECT F_NAME , L_NAME
-FROM EMPLOYEES
-WHERE B_DATE LIKE '197%';
