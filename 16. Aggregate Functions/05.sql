e14ffaf8afe895cb99bbe594f6bb0da79a27f3d2
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-10-07 19:36:26 +0530

    Create 05.sql

---
+++ 16. Aggregate Functions/05.sql
@@ -0,0 +1,4 @@
+select AVG(COST/QUANTITY) from PETRESCUE where ANIMAL = 'Dog';
+
+/*Query A5: Enter a function that displays the average cost of rescuing a dog.
+Hint - Bear in my the cost of rescuing one dog on day, is different from another day. So you will have to use and average of averages.
