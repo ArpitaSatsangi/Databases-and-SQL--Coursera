ccad7f0739d02d8c797a41b8c4a50dce7a57ec37
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-10-07 19:36:56 +0530

    Delete 16. Built-in functions/02.sql

--- 16. Built-in functions/02.sql
+++
@@ -1,4 +0,0 @@
-select sum(cost) as SUM_OF_COST 
-  from PETRESCUE;
-
-/*Query A2: Enter a function that displays the total cost of all animal rescues in the PETRESCUE table in a column called SUM_OF_COST.
