6d2ffb793719cd61e95a9aa8dd3e17766521fc67
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-10-08 18:42:03 +0530

    Create 05.sql

---
+++ 18. Date and Time Functions/05.sql
@@ -0,0 +1,4 @@
+
+select DATEDIFF(CURRENT_TIMESTAMP,RESCUEDATE) from PETRESCUE;
+/*Query C5: Enter a function that displays the length of time the animals have been rescued; 
+the difference between today’s date and the rescue date.
