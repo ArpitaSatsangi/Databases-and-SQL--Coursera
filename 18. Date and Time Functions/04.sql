9b4780fcd3651710933f04652089761a826e531e
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-10-08 18:41:25 +0530

    Create 04.sql

---
+++ 18. Date and Time Functions/04.sql
@@ -0,0 +1,3 @@
+
+select DATE_add(RESCUEDATE, INTERVAL 3 DAY) from PETRESCUE;
+/*Query C4: Animals rescued should see the vet within three days of arrivals. Enter a function that displays the third day from each rescue.
